3ffc663d53452148b11689c257a03c3b95a1e53c
Matthew Pacey <paceym@oregonstate.edu>
2023-02-12 14:19:01 -0800

    Completed part 1

--- hw2/univariate_tester.py
+++ hw2/univariate_tester.py
@@ -10,30 +10,19 @@ def s(x):
 # Task 1.2
 ################################
 
+
 # i gate
-w_ix = 100
-w_ih = 0
-b_i = 0
+w_ix, w_ih, b_i = 0, -20, 10
 
 # f gate
-w_fx = 0
-w_fh = 0
-b_f = 0
+w_fx, w_fh, b_f = -60, -60, 60
 
 # o gate
-w_ox = 0
-w_oh = 0
-b_o = 0
+w_ox, w_oh, b_o = 0, 0, 10
 
 # g
-w_gx = 0
-w_gh = 0
-b_g = 0
+w_gx, w_gh, b_g = 10, 0, 0
 
-#oh
-# 0,0,40
-# g
-# 40,0,0
 ################################
 
 # The below code runs through all length 14 binary strings and throws an error 
@@ -53,13 +42,17 @@ for X in itertools.product([0,1], repeat=14):
     c = f*c + i*g
     h = o*np.tanh(c)
   if np.sum(X)%2 != int(h>0.5):
-    print("Failure",cnt, X, int(h>0.5), np.sum(X)%2 == int(h>0.5))
+    raise Exception("Failure",cnt, X, int(h>0.5), np.sum(X)%2 == int(h>0.5))
     break
   if cnt % 1000 == 0:
     print(cnt)
 
+print(f'Success: {cnt}')
 
 def test_weights(w_ix, w_ih, b_i, w_fx, w_fh, b_f, w_ox, w_oh, b_o, w_gx, w_gh, b_g):
+  # used by the brute force method: take all inputs and try all values
+  # return count of successful entries up to that point (set status to dirty if any fail)
+  # if all passed, return clean status
   cnt = 0
   status = True
   pass_cnt = 0
@@ -79,26 +72,24 @@ def test_weights(w_ix, w_ih, b_i, w_fx, w_fh, b_f, w_ox, w_oh, b_o, w_gx, w_gh, 
       status = False
     else:
       pass_cnt += 1
-    # if cnt % 1000 == 0:
-    #   print(cnt)
 
   return status, pass_cnt
 
 
 def brute_force():
-  # yeah, best I can come up with right now
-  values = [0, 0.05, 0.1, 0.5, 1, 10, 50, 100, 1000]
+  # Starting with a few assumed values, use brute force guessing to get the rest
+  values = [-60, -40, -20, -10, 0, 10, 20, 40, 60]
   best = 0
 
-  for w_ix in values:
-    for w_ih in values:
-      for b_i in values:
+  for w_ix in [0]: # values:
+    for w_ih in [-20]: # values:
+      for b_i in [10]: #values:
         for w_fx in values:
           for w_fh in values:
             for b_f in values:
-              for w_ox in values:
-                for w_oh in values:
-                  for b_o in values:
+              for w_ox in [0]: # values:
+                for w_oh in [0]: # values:
+                  for b_o in [10]: # values:
                     for w_gx in values:
                       for w_gh in values:
                         for b_g in values:
@@ -113,8 +104,8 @@ def brute_force():
                             print(f'\t{w_ix=}, {w_ih=}, {b_i=}, {w_fx=}, {w_fh=}, {b_f=}, {w_ox=}, {w_oh=}, {b_o=}, {w_gx=}, {w_gh=}, {b_g=}')
                             best = cnt
 
-
-start = time.time()
-brute_force()
-end = time.time()
-print(f'Runtime: {end-start}')
+# optionally run the brute force method
+# start = time.time()
+# brute_force()
+# end = time.time()
+# print(f'Runtime: {end-start}')
